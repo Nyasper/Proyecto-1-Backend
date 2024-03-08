d0c78efcece535becb0dd1fd6cccb958993c1d1c
Nyasper <herreragonzalo39@gmail.com>
2024-03-08 20:23:39 -0300

    fixes

--- src/app.ts
+++ src/app.ts
@@ -4,24 +4,11 @@ import session from 'express-session';
 import routes from './routes';
 import cors from 'cors';
 
-const allowedDomains = [
-	'http://localhost:5173',
-	'https://proyecto-1-app-notas.vercel.app/',
-];
-const corsOptions = {
-	origin: function (origin: any, callback: any) {
-		if (allowedDomains.indexOf(origin) !== -1) {
-			callback(null, true);
-		} else {
-			callback(new Error('Not allowed by CORS'));
-		}
-	},
-	credentials: true,
-};
-
 const app = express();
 app.use(morgan('tiny'));
-app.use(cors(corsOptions));
+app.use(
+	cors({ origin: 'https://proyecto-1-app-notas.vercel.app', credentials: true })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
