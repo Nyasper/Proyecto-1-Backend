98c220f35ff6b3a578a05777fe5ae12b442e1eaf
Nyasper <herreragonzalo39@gmail.com>
2024-03-08 19:12:14 -0300

    ready to deploy

---
+++ src/api/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import morgan from 'morgan';
+import session from 'express-session';
+import cors from 'cors';
+import routes from '../routes';
+
+const app = express();
+app.use(morgan('tiny'));
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+	session({
+		secret: 'some secret',
+		saveUninitialized: false,
+		resave: false,
+		cookie: { maxAge: 60000 * 60 * 24 * 30 * 1 }, //1 month
+	})
+);
+
+const port = process.env.PORT ?? 3000;
+
+app.use(routes);
+app.get('/', (req, res) => {
+	res.send('Proyecto 1 Backend');
+});
+
+app.listen(port, () => {
+	console.log(`Server On http://localhost:${port}`);
+});
+
+export default app;
