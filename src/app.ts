e4fe8fb05083f8fa15d7e1aa0c108ff1899e026a
Nyasper <herreragonzalo39@gmail.com>
2024-03-08 20:08:49 -0300

    readyToDeploy

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,15 @@ import cors from 'cors';
 
 const app = express();
 app.use(morgan('tiny'));
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(
+	cors({
+		origin: [
+			'http://localhost:5173',
+			'https://proyecto-1-app-notas.vercel.app/',
+		],
+		credentials: true,
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
